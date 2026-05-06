3cd31d72383b458efacb51d490a7d298603fa127
nalogicyyy <2086879382@qq.com>
2026-05-06 21:06:47 +0800

    添加了学生链表类，并定义私有工具按学号查找结点

--- 学生管理系统.cpp
+++ 学生管理系统.cpp
@@ -10,7 +10,7 @@ class Student{
     int age;
     float score;
   public:
-    Student(string name,string id,string major,int age,float score){
+    Student(string name=" ",string id=" ",string major=" ",int age=0,float score=0.0){
         this->name=name;
         this->id=id;
         this->major=major;
@@ -23,8 +23,40 @@ class Student{
     }
 };
 class StudentNode{
-    Student date;
+public:
+    Student data;
     StudentNode* next;
-    StudentNode(Student student):date(student),next(nullptr){}
+    StudentNode(Student student):data(student),next(nullptr){}
 
+};
+class StudentList{
+private:
+    StudentNode* head;
+    int size;
+    //私有工具，按学号查找结点
+    StudentNode* findNodeById(const string& id) const {
+        StudentNode* p=head->next;
+        while (p!=nullptr){
+            if(p->data.id==id){
+                return p;
+            }
+            p=p->next;
+        }
+        return nullptr;
+    }
+public:
+StudentList(){
+    head=new StudentNode(Student());
+    size=0;
+}
+~StudentList(){
+    StudentNode* p=head;
+    while(p!=nullptr)
+    {
+        head=head->next;
+        delete p;
+        p=head;
+    }
+    size=0;
+}
 };
